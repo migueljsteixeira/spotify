72465c5d1e2e74be2c2825409395eba60e0cea00
Miguel Teixeira <64308962+migueljsteixeira@users.noreply.github.com>
2022-03-23 21:33:06 +0000

    Creating the sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,24 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return <div>Hello world Sidebar</div>;
+};
+
+export default Sidebar;
